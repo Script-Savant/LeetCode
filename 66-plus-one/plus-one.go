fc6f794563813aecf66dc6eef084d57652851bf9
Alex Kinuthia <alexkienjeku@gmail.com>
2025-03-13 02:42:27 +0300

    Time: 0 ms (100.00%) | Memory: 4 MB (30.46%) - LeetSync

--- 66-plus-one/plus-one.go
+++ 66-plus-one/plus-one.go
@@ -1,5 +1,4 @@
 func plusOne(digits []int) []int {
-    fmt.Println(digits)
     n := len(digits)
     if n == 0{
         return []int {}
@@ -8,14 +7,11 @@ func plusOne(digits []int) []int {
     for i := n-1; i >= 0; i--{
         if digits[i] < 9 {
             digits[i]++
-            fmt.Println(digits)
             return digits
         }
         digits[i] = 0
-        fmt.Println(digits)
     }
 
     digits = append([]int {1}, digits...)
-    fmt.Println(digits)
     return digits
 }
