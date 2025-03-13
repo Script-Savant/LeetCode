9b406a5dae37b7af7007cbf2e654e6f7b23c9a6a
Alex Kinuthia <alexkienjeku@gmail.com>
2025-03-14 00:02:53 +0300

    Time: 0 ms (100.00%) | Memory: 3.9 MB (95.07%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,20 @@
+func strStr(haystack string, needle string) int {
+    n := len(haystack)
+    m := len(needle)
+
+    if m == 0 {
+        return 0
+    }
+
+    if m > n {
+        return -1
+    }
+
+    for i := 0; i <= n-m; i++{
+        if haystack[i:i+m] == needle{
+            return i
+        }
+    }
+
+    return -1
+}
